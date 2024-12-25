4f63987878bf47903ccef366a3a20b8b36b402f3
Sten Scheifel <sten.scheifel@gmail.com>
2024-12-25 21:28:49 +0100

    first commit, header completed design

---
+++ webapp/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-title">My Blog</div>
+        <nav className="App-nav">
+          <div>Home</div>
+          <button className="App-login-btn">Login</button>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
