512c85f5a242d939f0c73b2e395ba7bcb46b28d9
Sten Scheifel <sten.scheifel@gmail.com>
2024-12-29 13:22:38 +0100

    added new resources to get artcile by id. refactpred a bit

--- api/server.js
+++ api/server.js
@@ -4,22 +4,28 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
+const articles = [
+  {
+    id: 1,
+    title: "First article",
+    content:
+      "Some article text here.Some article text here.Some article text here",
+  },
+  {
+    id: 2,
+    title: "Second article",
+    content: "Working on a project",
+  },
+];
 
 app.get("/articles", (req, res) => {
-  const articles = [
-    {
-      id: 1,
-      title: "First article",
-      content:
-        "Some article text here.Some article text here.Some article text here",
-    },
-    {
-      id: 2,
-      title: "Second article",
-      content: "Working on a project",
-    },
-  ];
   res.send(articles);
+});
+
+app.get("/articles/:articleId", (req, res) => {
+  const articleId = req.params.articleId;
+  const article = articles.find((art) => art.id === Number(articleId));
+  res.send(article);
 });
 
 app.post("/create", (req, res) => {
