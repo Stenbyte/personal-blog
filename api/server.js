64270af6af5b830e84a5177fce1501c6f482d011
Sten Scheifel <sten.scheifel@gmail.com>
2024-12-29 11:54:26 +0100

    added nodemon, create basic routes to fetch and create article

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+app.get("/articles", (req, res) => {
+  const articles = [
+    {
+      id: 1,
+      title: "First article",
+      content:
+        "Some article text here.Some article text here.Some article text here",
+    },
+    {
+      id: 2,
+      title: "Second article",
+      content: "Working on a project",
+    },
+  ];
+  res.send(articles);
+});
+
+app.post("/create", (req, res) => {
+  console.log("create request");
 });
 
 app.listen(port, () => {
