cdaabdc0d282d91e14cae5f8094c2a056fc3d5eb
Sten Scheifel <sten.scheifel@gmail.com>
2025-01-05 11:40:19 +0100

    added error checks

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -3,6 +3,7 @@ import { getDb } from "../db.js";
 
 export async function createArticle(req) {
   const newArticleToAdd = req.body;
+  throwIfArticleHasMissingFields(newArticleToAdd);
   const articlesCollection = await getDb("articles");
 
   const newArticle = await articlesCollection.insertOne(newArticleToAdd);
@@ -19,10 +20,19 @@ export async function getArticles() {
 
 export async function getArticle(req) {
   const articleId = req.params.articleId;
+  if (!ObjectId.isValid(articleId)) {
+    throw Error("Article id must be type of ObjectId");
+  }
   const articlesCollection = await getDb("articles");
 
   const getArticleById = await articlesCollection.findOne({
     _id: ObjectId.createFromHexString(articleId),
   });
   return getArticleById;
+}
+
+function throwIfArticleHasMissingFields(article) {
+  if (!article || article.title.length === 0 || article.content.length === 0) {
+    throw Error("Article fields must be filled up");
+  }
 }
