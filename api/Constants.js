acd63a91d833e5d280e089ed33df2a4485a2f999
Sten Scheifel <sten.scheifel@gmail.com>
2025-01-11 18:00:11 +0100

    added constants for audit log

---
+++ api/Constants.js
@@ -0,0 +1,10 @@
+const Constants = {
+  VIEWV_ARTICLES: "VIEW_ARTICLES",
+  VIEW_ARTCILE: "VIEW_ARTICLE",
+  CREATE: "CREATE",
+  EDIT: "EDIT",
+  DELETE: "DELETE",
+  CREATE_USER: "CREATE_USER",
+};
+
+module.exports = Constants;
