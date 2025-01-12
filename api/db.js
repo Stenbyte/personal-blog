4dd3fb343f1ecfefc03a00405d87526513b37b7d
Sten Scheifel <sten.scheifel@gmail.com>
2025-01-12 14:57:44 +0100

    added mongoose connection events

--- api/db.js
+++ api/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const error = require("mongoose/lib/error");
+const { customError } = require("./middleware/errorHandler");
 
 const connectionString = `mongodb://127.0.0.1:27017/masterDb`;
 
@@ -7,12 +8,30 @@ let connection;
 async function connectToDb() {
   try {
     if (!connection) {
+      mongoose.connection.on("connected", () =>
+        console.log("✅ Connected to MongoDB")
+      );
+
+      mongoose.connection.on("disconnected", async () => {
+        console.warn("⚠️ MongoDB Disconnected. Reconnecting...");
+        await connectToDb();
+      });
+
+      mongoose.connection.on("error", (error) =>
+        console.error("❌ MongoDB Connection Error:", error)
+      );
+
+      mongoose.connection.on("close", () =>
+        console.log("🔌 Connection to MongoDB closed")
+      );
       connection = await mongoose.connect(connectionString);
       process.env.masterDbReady = "true";
     }
+
     return connection;
   } catch (error) {
-    throw Error(error);
+    mongoose.connection.close();
+    throw customError(error);
   }
 }
 
